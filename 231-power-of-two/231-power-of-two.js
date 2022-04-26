6ae05292c777f07d845aef815c40cea144b453dd
Winston Pantelakos <winstonspantelakos@gmail.com>
2022-04-26 15:03:33 -0400

    Time: 82 ms (68.71%), Space: 43.6 MB (62.34%) - LeetHub

---
+++ 231-power-of-two/231-power-of-two.js
@@ -0,0 +1,30 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfTwo = function(n) {
+    if (n === 1) return true;
+    if (n === 0) return false;
+    if (n % 2 === 1) return false;
+    if (n > 0) {
+        while (n > 1) {
+            n /= 2;
+        }
+        if (n === 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    if (n < 0) {
+        // while (n < -1) {
+        //     n *= 2;
+        // }
+        // if (n === -1) {
+        //     return true;
+        // } else {
+        //     return false;
+        // }
+        return false;
+    }
+};
